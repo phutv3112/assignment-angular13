656c5eaccabef2d4107dda7bc66521f7c87232f0
vanphutran311 <vanphutran311@gmail.com>
2024-08-27 15:30:33 +0700

    done assignment 9

--- src/app/pipes/enum-description.pipe.spec.ts
+++ src/app/pipes/enum-description.pipe.spec.ts
@@ -1,21 +1,51 @@
-import { BaseTypeEnum } from '../enums/base-type.enum';
 import { EnumDescriptionPipe } from './enum-description.pipe';
+import { BaseTypeEnum } from '../enums/base-type.enum';
 
 describe('EnumDescriptionPipe', () => {
-  let pipe: EnumDescriptionPipe
+  let pipe: EnumDescriptionPipe;
+
   beforeEach(() => {
     pipe = new EnumDescriptionPipe();
   });
-  it('create an instance', () => {
-    const pipe = new EnumDescriptionPipe();
+
+  it('should create an instance', () => {
     expect(pipe).toBeTruthy();
   });
-  it('should transform enum code to name', () => {
+
+  it('should return name if item is found', () => {
     const mockEnum: BaseTypeEnum[] = [
       { code: '1', name: 'Type 1' },
       { code: '2', name: 'Type 2' },
     ];
     expect(pipe.transform('1', mockEnum)).toBe('Type 1');
-    expect(pipe.transform('3', mockEnum)).toBe('');
+  });
+
+  it('should return null if code is provided but item is not found', () => {
+    const mockEnum: BaseTypeEnum[] = [
+      { code: '1', name: 'Type 1' },
+      { code: '2', name: 'Type 2' },
+    ];
+    expect(pipe.transform('3', mockEnum)).toBeNull();
+  });
+
+  it('should return null if code is empty', () => {
+    const mockEnum: BaseTypeEnum[] = [
+      { code: '1', name: 'Type 1' },
+      { code: '2', name: 'Type 2' },
+    ];
+    expect(pipe.transform('', mockEnum)).toBeNull();
+  });
+
+  it('should return null if code is null', () => {
+    const mockEnum: BaseTypeEnum[] = [
+      { code: '1', name: 'Type 1' },
+      { code: '2', name: 'Type 2' },
+    ];
+    expect(pipe.transform(null, mockEnum)).toBeNull();
+  });
+
+  it('should return null if code is provided but enumItems list is empty', () => {
+    const mockEnum: BaseTypeEnum[] = [];
+    expect(pipe.transform('1', mockEnum)).toBeNull();
   });
 });
--- src/app/pipes/enum-description.pipe.ts
+++ src/app/pipes/enum-description.pipe.ts
@@ -1,11 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { BaseTypeEnum } from '../enums/base-type.enum';
+
 @Pipe({
   name: 'enumDescription',
 })
 export class EnumDescriptionPipe implements PipeTransform {
-  transform(code: string, enumItems: BaseTypeEnum[]): string {
-    const matchedItem = enumItems.find((item) => item.code === code);
-    return matchedItem ? matchedItem.name : '';
+  transform(code: string | null, enumItems: BaseTypeEnum[]): string | null {
+    if (!code || !enumItems || enumItems.length === 0) {
+      return null;
+    }
+    const item = enumItems.find((e) => e.code === code);
+    return item ? item.name : null;
   }
 }
